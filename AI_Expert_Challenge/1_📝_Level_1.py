ad3e8996f760076247041e4cf65750731647b50d
MD.Sifat Hossain <142619926+decodersifat@users.noreply.github.com>
2024-10-22 13:07:06 +0600

    Update 1_📝_Level_1.py

--- AI_Expert_Challenge/1_📝_Level_1.py
+++ AI_Expert_Challenge/1_📝_Level_1.py
@@ -11,10 +11,28 @@ st.set_page_config(page_title="AI Expert Challenge !!!", page_icon=":pencil2:", 
 
 page_bg_img = f"""
 <style>
-[data-testid="stAppViewContainer"] 
-{
-    background-color:#011516
-}
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-size: 180%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
 </style>
 """
 
